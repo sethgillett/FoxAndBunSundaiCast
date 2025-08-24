c8c7bc3b13f27bcf4ac17a4a9e776fc3a967e578
Alice Marbach <alice.marbach@gmail.com>
2025-08-24 16:49:37 -0400

    update main.py to run whole pipeline

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from parse_projects import parse_relevant_json, pull_sundai_projects
 from combine_whole_transcript import combine_whole_transcript
+from transcript_to_speech_assets import transcript_to_speech
 import json 
+from merge_all_audio_files import merge_mp3_files
 
 def run_pipeline():
+    print("Pulling projects...")
     projects = parse_relevant_json(pull_sundai_projects())
+    print("Generating transcript...")
     transcript = combine_whole_transcript(projects[:10])
-    return transcript
+    print("Saving transcript...")
+    with open("transcript.json", "w") as f:
+        f.write(json.dumps(transcript))
+    print("Converting transcript to speech...")
+    transcript_to_speech(json.load(open("transcript.json"))["dialogue"])
+    print("Merging audio files...")
+    merge_mp3_files("./audio_assets", "merged.mp3")
+    print("Done")
+    
 
 if __name__ == "__main__":
     output = run_pipeline()
-    print(json.dumps(output))
